7f123fa4a8866e461806359732ad1e1d9179b8af
aagogue <aagogue@iut.univ-paris8.fr>
2024-11-18 17:31:50 +0100

    clean du Marchand

--- src/main/java/universite_paris8/iut/EtrangeEtrange/modele/Acteurs/Entite/PNJ/Interagisable/Marchand.java
+++ src/main/java/universite_paris8/iut/EtrangeEtrange/modele/Acteurs/Entite/PNJ/Interagisable/Marchand.java
@@ -1,51 +1,33 @@
 package universite_paris8.iut.EtrangeEtrange.modele.Acteurs.Entite.PNJ.Interagisable;
 
-
 import universite_paris8.iut.EtrangeEtrange.modele.Acteurs.Dropable;
 import universite_paris8.iut.EtrangeEtrange.modele.Acteurs.Entite.Offensif;
 import universite_paris8.iut.EtrangeEtrange.modele.Acteurs.Entite.PNJ.NPEs;
 import universite_paris8.iut.EtrangeEtrange.modele.Acteurs.Entite.PNJ.Patterns.ConditionsDecorateur.ConditionDelaieRespecter;
 import universite_paris8.iut.EtrangeEtrange.modele.Acteurs.Entite.PNJ.Patterns.Pattern;
 import universite_paris8.iut.EtrangeEtrange.modele.Interaction.Action.ActionVendre;
-
 import universite_paris8.iut.EtrangeEtrange.modele.Interaction.Action.Soigner;
 import universite_paris8.iut.EtrangeEtrange.modele.Interaction.Prompte.Prompt;
-
-
 import universite_paris8.iut.EtrangeEtrange.modele.Objet.Armes.Epee;
 import universite_paris8.iut.EtrangeEtrange.modele.Objet.Armes.Arc;
 import universite_paris8.iut.EtrangeEtrange.modele.Objet.Contenant.Sac;
-
 import universite_paris8.iut.EtrangeEtrange.modele.Objet.Dommageable;
-import universite_paris8.iut.EtrangeEtrange.modele.Objet.Objet;
 import universite_paris8.iut.EtrangeEtrange.modele.Objet.Soins.Potion;
-import universite_paris8.iut.EtrangeEtrange.modele.stockage.DropAuSol;
-import universite_paris8.iut.EtrangeEtrange.modele.stockage.Emplacement;
 import universite_paris8.iut.EtrangeEtrange.modele.Utilitaire.Direction;
 import universite_paris8.iut.EtrangeEtrange.modele.Utilitaire.Hitbox;
-import universite_paris8.iut.EtrangeEtrange.modele.Utilitaire.Position;
-
-import java.util.ArrayList;
 
 public class Marchand extends NPEs implements Dropable {
 
-    private int cycle;
-    private Sac sac;
-
+    private final Sac sac;
     private Prompt prompt;
 
     public Marchand(double x, double y, Direction direction) {
-        super(x, y, direction, 10, 10 , 10, 0.5, new Hitbox(0.5,0.5));
-        this.cycle = 0;
+        super(x, y, direction, 10, 10, 10, 0.5, new Hitbox(0.5, 0.5));
         this.sac = new Sac();
-
         initPrompt();
-
     }
 
-
-    public void genereMarchandises()
-    {
+    private void genereMarchandises() {
         sac.ajoutItem(new Epee());
         sac.ajoutItem(new Arc());
         sac.ajoutItem(new Potion());
@@ -53,7 +35,7 @@ public class Marchand extends NPEs implements Dropable {
 
     @Override
     protected Pattern initPattern() {
-        return new ConditionDelaieRespecter(this::genereMarchandises,5000);
+        return new ConditionDelaieRespecter(this::genereMarchandises, 5000);
     }
 
     @Override
@@ -62,9 +44,7 @@ public class Marchand extends NPEs implements Dropable {
     }
 
     @Override
-    public void dropApresMort() {
-
-    }
+    public void dropApresMort() {}
 
     @Override
     public boolean estUnEnemie() {
@@ -72,43 +52,31 @@ public class Marchand extends NPEs implements Dropable {
     }
 
     @Override
-    public  void subitAttaque(Dommageable causeDegat, Offensif entiteOffensif) {
-
-    }
+    public void subitAttaque(Dommageable causeDegat, Offensif entiteOffensif) {}
 
     private void initPrompt() {
-        Prompt racine = new Prompt("Bonjour ! Que vous ramene ici ?", null);
+        Prompt racine = new Prompt("Bonjour ! Que vous ramène ici ?", null);
 
         Prompt reponseRacine1 = new Prompt("Voici ce que je propose.", new ActionVendre(this));
+        Prompt reponseRacine2 = new Prompt("Vous avez entendu parler du monstre qui rôde dans les coins", null);
 
-        Prompt reponseRacine2 = new Prompt("Vous avez entendu parlé du monstre qui rôde dans dans les coins", null);
+        racine.ajoutPrompt(reponseRacine1, "J'aimerais marchander un peu avec vous");
+        racine.ajoutPrompt(reponseRacine2, "J'aimerais parler un peu...");
 
-        racine.ajoutPrompt(reponseRacine1, "J'aimerais marchander un peu avec avec vous");
-        racine.ajoutPrompt(reponseRacine2, "J'aimerais parler un peu..");
+        Prompt reponseReponseRacine2 = new Prompt("Faites attention... D'ailleurs, attendez, je vais vous soigner !", new Soigner(monde.getJoueur()));
+        reponseRacine2.ajoutPrompt(reponseReponseRacine2, "");
 
-        Prompt reponseReponceRacine2 = new Prompt("Faite attention...   D'ailleur, attendez je vais vous soigner !", new Soigner(monde.getJoueur()));
-        reponseRacine2.ajoutPrompt(reponseReponceRacine2, "");
-
-        prompt = racine;
+        this.prompt = racine;
     }
-
 
     @Override
-    public void drop() {
-        for (Emplacement<Objet> objets : sac.getInventaire()) {
-            ArrayList<Objet> obs = objets.enleverToutLesObjets();
-
-            for (Objet objet : obs) {
-                monde.ajouterDropAuSol(new DropAuSol(objet, new Position(position.getX(), position.getY())));
-            }
-        }
-    }
+    public void drop() {}
 
     public Sac getMarchandise() {
         return sac;
     }
 
-    public Prompt getPrompt(){
+    public Prompt getPrompt() {
         return prompt;
     }
 }
